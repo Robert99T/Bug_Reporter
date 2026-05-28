54783d5daf7681f70afa84372029d4a458e1f689
CaesarHD <stir.cezar@gmail.com>
2026-05-28 21:27:20 +0300

    started

---
+++ backend/src/main/java/com/bug/bug_reporter/service/NotificationClient.java
@@ -0,0 +1,34 @@
+package com.bug.bug_reporter.service;
+
+import com.bug.bug_reporter.dto.NotificationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationClient {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${notification.service.url}")
+    private String notificationServiceUrl;
+
+    public void sendBanNotification(String email, String phone, String message) {
+        NotificationRequest request = new NotificationRequest(email, phone, message);
+        try {
+            restTemplate.exchange(
+                    notificationServiceUrl + "/notifications",
+                    HttpMethod.POST,
+                    new HttpEntity<>(request),
+                    String.class
+            );
+        } catch (RestClientException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
